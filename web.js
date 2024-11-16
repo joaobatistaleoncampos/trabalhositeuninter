ff75b7fab71e223e8b862a4aed7a640568091e33
joaobatistaleoncampos <instrutorjoaoleon@gmail.com>
2024-11-16 16:00:07 -0300

    Add files via upload

---
+++ web.js
@@ -0,0 +1,11 @@
+//const nome = window.prompt('nome completo')
+ //window.alert('seja bem vindo(a)'+nome)
+/*
+ let botao = document.querySelector("#botao");
+ botao.style.background="orange";
+
+ botao.addEventListener("mouseover", trocaVerde);
+   
+  function trocaVerde( ) {
+   botao.style.background="green";*/
+ 
